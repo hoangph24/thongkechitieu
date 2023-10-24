ea3c4a4c20ca1b543910c8dad8e32c4850becba5
hoangph24 <54206911+hoangph24@users.noreply.github.com>
2023-10-25 00:15:10 +0700

    Update date column and change order of columns

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 bot.onText(/\/add (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
 
-    if (!match[1].includes('|')) {
-        bot.sendMessage(chatId, 'Vui lòng nhập đúng định dạng.' + '\n\n' + 'Ví dụ:\n```\n/add Danh mục|Số tiền|Note\n```', {
+    if (!match[1].includes(',')) {
+        bot.sendMessage(chatId, 'Vui lòng nhập đúng định dạng.' + '\n\n' + 'Ví dụ:\n```\n/add Danh mục,Số tiền,Note\n```', {
             parse_mode: 'Markdown'
         });
         return;
@@ -17,12 +17,13 @@ bot.onText(/\/add (.+)/, (msg, match) => {
     bot.sendChatAction(chatId, 'typing');
 
     const resp = match[1];
-    const values = resp.split('|');
+    const values = resp.split(',');
 
     const url = new URL(process.env.WEBHOOK_URL);
     url.searchParams.append('Danh mục', values[0]);
     url.searchParams.append('Số tiền', values[1]);
     url.searchParams.append('Note', values[2]);
+    url.searchParams.append('Ngày', (new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok' })).toLocaleDateString('en-GB')).replace(/\//g, '-'));
 
     fetch(url)
         .then(res => res.json())
